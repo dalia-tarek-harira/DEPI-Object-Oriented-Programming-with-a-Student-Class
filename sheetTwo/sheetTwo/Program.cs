cfd107d11584e1b0a286db0c45fff12a00a14f78
dalia-tarek-harira <daliaharira3@gmail.com>
2025-08-19 16:06:21 +0300

    Add files via upload

---
+++ sheetTwo/sheetTwo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace sheetTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student studentOne = new Student("Dalia Tarek", 21, 100);
+            studentOne.DisplayInfo();
+
+
+            Student studentTwo = new Student("Rania Tarek", 15, 99);
+            studentTwo.DisplayInfo();
+            
+        }
+    }
+}
---
+++ sheetTwo/sheetTwo/Student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sheetTwo
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Grade { get; set; }
+
+        public Student(string name, int age, double grade) 
+        {
+            Name = name;
+            Age = age;
+            Grade = grade;
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Student Name Is: {Name}\nStudent Age Is: {Age}\nStudent Grade Is: {Grade}\n\n\n");
+        }
+    }
+}
